79d44f6bedc9ed58ba335bc71292bf3c02d391aa
Aleksandr Vishnyakov <av@virtoway.com>
2017-08-01 20:18:03 +0500

    [publish] 2.12.1

--- Paypal.ExpressCheckout/Properties/AssemblyInfo.cs
+++ Paypal.ExpressCheckout/Properties/AssemblyInfo.cs
@@ -1,35 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Paypal.ExpressCheckout.PaymentGatewayModule")]
+[assembly: AssemblyTitle("Paypal.ExpressCheckout")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Paypal.ExpressCheckout.PaymentGatewayModule")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0aac0896-55a2-4004-aacb-760e92a484bd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("2.12.0.0")]
-[assembly: AssemblyFileVersion("2.12.0.0")]
